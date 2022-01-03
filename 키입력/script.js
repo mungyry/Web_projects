b895636d2f8588ed94408b3216b190ff95e693da
mungyry <mungyry1@naver.com>
2022-01-03 15:09:57 +0900

    2022년1월3일오후

---
+++ 키입력/script.js
@@ -0,0 +1,22 @@
+// 입력할 태그 객체
+const insert = document.getElementById('insert');
+
+// 현재화면에서 키입력시 이벤트 발생
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+  <div class="key">
+  ${e.key} 
+  <small>event.key</small> 
+</div>
+
+<div class="key">
+  ${e.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${e.code}
+  <small>event.code</small>
+</div>
+  `;
+});
